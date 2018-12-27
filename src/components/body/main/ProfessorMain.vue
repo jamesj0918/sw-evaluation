6be7a5648a13482230b22eccf9cfa48987c1bc07
leewoojin <mingwoojin@naver.com>
2018-12-27 19:35:05 +0900

    LectureList, Main css

--- src/components/body/main/ProfessorMain.vue
+++ src/components/body/main/ProfessorMain.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="ProfessorMain">
         <router-view>
         </router-view>
     </div>
@@ -12,5 +12,7 @@
 </script>
 
 <style scoped>
-
+    #ProfessorMain {
+        width: 100%; height: 100%;
+    }
 </style>
